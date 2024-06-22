5cb17d03b1aa936f9d9789986d13d9c983260ab0
William-ops-crypto <william.s.91@hotmail.com>
2024-06-22 20:27:46 -0300

    Corrigindo Url da api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
    
-   baseURL:"https://c07d-18-206-58-63.ngrok-free.app/",
+   baseURL:"https://5c8d-18-206-58-63.ngrok-free.app/",
    headers: {
       'ngrok-skip-browser-warning': 'application/x-www-form-urlencoded',
     
