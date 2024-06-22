dff0c3483407290f52a88806fc08a0aa529ab369
William-ops-crypto <william.s.91@hotmail.com>
2024-06-22 19:56:21 -0300

    corrigindo bug

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 const api = axios.create({
    
-   baseURL:"https://c07d-18-206-58-63.ngrok-free.app/"
+   baseURL:"https://c07d-18-206-58-63.ngrok-free.app/",
+   headers: {
+      'ngrok-skip-browser-warning': 'application/x-www-form-urlencoded',
+    
+    }
  
 });
 
