9d4482c9970fc9eec1827dea51738296640afbb8
William-ops-crypto <william.s.91@hotmail.com>
2024-06-27 19:08:22 -0300

    Atualizando header e footer

--- src/componets/Footer/index.js
+++ src/componets/Footer/index.js
@@ -1,22 +1,37 @@
 import React from 'react'
 import "./styles.css";
 import {
-
-    Box,
+    Flex,
     Center,
     
     
   } from '@chakra-ui/react'
-
+import  {  SocialIcon  }  from  'react-social-icons'
 
 const Footer = () => {
 
 
     return(
 
-        <Center className="Footer">
-        TRodapé
-        </Center>
+        
+               <Flex bg='teal' w={'100%'} >
+       
+      
+               <Center className="Footer" >
+
+               <SocialIcon  className = "Icone"  href='https://www.linkedin.com/in/william-santos-da-silva/' target="_blank"  url="https://linkedin.com" />
+             
+               <SocialIcon className = "Icone"  href='https://github.com/William-ops-crypto' target="_blank"  url="https://github.com" />
+              
+               <SocialIcon className = "Icone"  href='https://api.whatsapp.com/send/?phone=5521995915256&text&type=phone_number&app_absent=0' target="_blank"  url="https://whatsapp.com" />
+               
+
+              
+                
+       </Center>
+       
+       </Flex>
+       
 )
 
 
--- src/componets/Header/index.js
+++ src/componets/Header/index.js
@@ -1,24 +1,41 @@
 import React from 'react'
 
 import {
-
-    Box,
+    Flex,
+    Text,
     Center,
+    Image
     
     
-  } from '@chakra-ui/react'
+  } from '@chakra-ui/react';
+  import { Link } from '@chakra-ui/react';
+ 
 
 
 const Header = () => {
 
 
     return(
-
-        <Center  borderRadius='md'  bg='teal' w='100%' p={20} alignItems={'center'}>
-                 <h1>Meu Portifólio</h1> 
-        </Center>
+        <Flex bg='teal' w={'100%'} >
        
+        <Center  margin={5}   p={10} borderRadius={30} fontStyle={'oblique'} fontSize={20} color="Black" >
+                 
+                     <Image
+                     
+                     marginLeft={10}
+                     marginRight={10}
+                         borderRadius={100}
+                          boxSize='50px'
+                         
+                          src='https://scontent.fcaw5-1.fna.fbcdn.net/v/t1.6435-9/118386212_3457993920913484_454249750073191862_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=1d70fc&_nc_eui2=AeGcVkN6MWHaNGc9Lw-egj3vqKTkh038B8GopOSHTfwHwVqz6wDg--i7G12WT5U5_m5SRs-eNHW2GyyzxdobSoZM&_nc_ohc=-LYOAq7Bsd0Q7kNvgE7P3AQ&_nc_ht=scontent.fcaw5-1.fna&oh=00_AYDdi8ifCvRyszKQPWJMp67NEa_DqwABbAkd4pN6HPCSng&oe=66A40F7B'
+                          alt='Foto will'
+                            />   
+      
+        <Text fontSize='2xl'>William Santos</Text>
 
+        </Center>
+        
+        </Flex>
 
 
     )
